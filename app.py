f2b5d6444f55d9496e421e851b67c0b1ea3462f7
zingmovie21 <133083254+zingmovie21@users.noreply.github.com>
2024-12-02 17:15:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,4 +104,5 @@ async def root():
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import uvicorn
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
